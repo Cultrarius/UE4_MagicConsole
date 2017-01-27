9717170f41593fb2cc83773f1cae17a370380e94
Michael Galetzka <galetzka.michael@gmail.com>
2017-01-27 21:52:17 +0100

    added plugin compile script

---
+++ compile.bat
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\Epic Games\4.14\Engine\Build\BatchFiles\RunUAT.bat" BuildPlugin -Plugin=D:\Unreal\Plugins\MagicConsole\Plugins\ConsoleEnhanced\ConsoleEnhanced.uplugin -Package=D:\Unreal\Plugins\MagicConsole\Packaged\EnhancedOutputLog -Rocket
