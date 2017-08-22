0cebacf8a2d784a1bb56e170cb78b94dd5294f3e
Michael Galetzka <galetzka.michael@gmail.com>
2017-08-22 07:58:39 +0200

    Updated for 4.17

--- compile.bat
+++ compile.bat
@@ -1,4 +1,6 @@
 :: "C:\Program Files (x86)\Epic Games\4.14\Engine\Build\BatchFiles\RunUAT.bat" BuildPlugin -Plugin=D:\Unreal\Plugins\MagicConsole\Plugins\ConsoleEnhanced\ConsoleEnhanced.uplugin -Package=D:\Unreal\Plugins\MagicConsole\Packaged\EnhancedOutputLog -Rocket
 
 
-"C:\Program Files\Epic Games\UE_4.16\Engine\Build\BatchFiles\RunUAT.bat" BuildPlugin -Plugin=D:\Unreal\Plugins\MagicConsole\Plugins\ConsoleEnhanced\ConsoleEnhanced.uplugin -Package=D:\Unreal\Plugins\MagicConsole\Packaged\EnhancedOutputLog -Rocket
+:: "C:\Program Files\Epic Games\UE_4.16\Engine\Build\BatchFiles\RunUAT.bat" BuildPlugin -Plugin=D:\Unreal\Plugins\MagicConsole\Plugins\ConsoleEnhanced\ConsoleEnhanced.uplugin -Package=D:\Unreal\Plugins\MagicConsole\Packaged\EnhancedOutputLog -Rocket
+
+E:\GameDev\Unreal\UE_4.17\Engine\Build\BatchFiles\RunUAT.bat BuildPlugin -Plugin=D:\Unreal\Plugins\MagicConsole\Plugins\ConsoleEnhanced\ConsoleEnhanced.uplugin -Package=D:\Unreal\Plugins\MagicConsole\Packaged\EnhancedOutputLog -Rocket
